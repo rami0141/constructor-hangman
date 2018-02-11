6c7e0f9e8e76b16fdde092057f3eed459fbcdf34
Cristina Zhang <rami0141@umn.edu>
2018-02-11 13:40:47 -0600

    starting code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js: The file containing the logic for the course of the game,
 //  which depends on Word.js and:
-
+var word = require('./word');
 
 // Randomly selects a word and uses the Word constructor to store it
 // Prompts the user for each guess and keeps track of the user's remaining guesses
--- letter.js
+++ letter.js
@@ -4,19 +4,34 @@
 // will call that function automatically whenever casting that object to a 
 // string (check out this example: https://jsbin.com/facawetume/edit?js,console)
 
-
-
+var inquirer = required('inquirer');
 // Letter.js: Contains a constructor, Letter.
 // This constructor should be able to either display an underlying character 
 //or a blank placeholder (such as an underscore), depending on whether or not 
 //the user has guessed the letter. That means the constructor should define:
 
+function Letter(letter) {
+	// A string value to store the underlying character for the letter
+	this.letter = letter;
+	// A boolean value that stores whether that letter has been guessed yet
+	this.guessesSoFar = true;
+	// A function that returns the underlying character if the letter has been guessed, 
+	//or a placeholder (like an underscore) if the letter has not been guessed
+	this.underlyingCharacter = function() {
+		if(this.letter === " ") {
+			this.guessesSoFar = true;
+			return " ";
+		} 
+	}; //end of underlyingCharacter function
 
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been 
-//guessed, or a placeholder (like an underscore) if the letter has not been 
-//guessed
-// A function that takes a character as an argument and checks it against the 
-//underlying character, updating the stored boolean value to true if it was 
-//guessed correctly
+	this.check = function(letter) {
+	// A function that takes a character as an argument and checks it against the 
+	//underlying character, updating the stored boolean value to true if it was guessed correctly
+		if(this.guessesSoFar === false) {
+			return " _ ";
+		}
+	}//end of check function
+}//end of constructor Letter
+
+// exporting letter.js
+module.exports = letter;
