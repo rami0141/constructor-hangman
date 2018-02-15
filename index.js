279cf672447fb174bd7a0799279fb60ae933abe9
Cristina Zhang <rami0141@umn.edu>
2018-02-14 22:01:44 -0600

    comments added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Word = require('./Word.js');
 var RWord = function (wordList) {
     this.Words = wordList
 
-
+    // This fuction pick a random word from the word list
     this.GenrandomWord = function() {
         var rw = this.Words[Math.floor(Math.random() * this.Words.length)];
         var rwo = new Word(rw);
--- word.js
+++ word.js
@@ -1,10 +1,13 @@
 var Word = function (inputword) {
     this.cword = inputword;
+    // split() will split up one word into letters "pitbull" to "p", "i", "t", "b", "u", "l", "l"
     this.correct = this.cword.split('');
+    // array where you push letters
     this.display = [];
 
     this.initiate = function () {
         for (var i = 0; i < this.correct.length; i++) {
+            // this will add underscores to the display array instead of the letters
             this.display.push("_")
         }
     }
@@ -26,40 +29,3 @@ module.exports = Word;
 
 
 
-//Word.js: Contains a constructor, Word that depends on the Letter constructor. 
-//This is used to create an object representing the current word the user is 
-//attempting to guess. That means the constructor should define:
-//take in info from letter.js
-// var letter = require('./Letter');
-
-// function Word(word) {
-// 	//setting a property called word and is equal to word which will be passed through the function
-// 	//will take words from array
-// 	this.word = word;
-
-// 	//an empty array of letters that are in each word
-// 	this.newALetterArray = []; 
-// 	this.userGuessArray = [];
-	
-// 	//Creates a Letter object for each letter in the current word.
-// 	this.pushToArray = function() {
-// 		for (var i = 0; i < this.word.length; i++){
-// 			//charAt() method returns the character at the specified index in a string.
-// 			this.letter = new Letter(word.charAt(i))
-// 			//pushes the letter into the newLetterArray
-//     		this.newALetterArray.push(this.letter);
-// 		}
-// 	}
-// // A function that returns a string representing the word. This should 
-// //call the function on each letter object (the first function defined in Letter.js) 
-// //that displays the character or an underscore and concatenate those together.
-
-// 	this.character = function(){
-
-	
-// 	}
-// };
-
-// // A function that takes a character as an argument and calls the guess function 
-// //on each letter object (the second function defined in Letter.js)
-// module.exports = Word; 
